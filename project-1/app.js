f468080da164acd09dec229036a21684ecf4fe72
Manideep Reddy Aliminati <manideepreddyaliminati@manideepslaptop.dhcp.asu.edu>
2024-02-15 15:24:57 -0700

    Adding support for file uploads in post requests using multer.

--- project-1/app.js
+++ project-1/app.js
@@ -1,34 +1,41 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require("fs");
+const path = require("path");
 
-const express = require('express')
-const morgan = require('morgan')
+const express = require("express");
+const morgan = require("morgan");
+const multer = require("multer");
 
-const port = 3000
-const app = express()
+const port = 3000;
+const app = express();
 
 // Middlewares
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
 
-let data = fs.readFileSync(path.join(`${__dirname}`, '/data/classification_results.csv'), 'utf8');
-data = data.split("\n")
-let classification = {}
-for(let i=0; i<data.length; i++){
-    let fileName = data[i].split(",")[0]
-    let value = data[i].split(",")[1].trim()
-    classification[fileName] = value
+// Configure Multer for file uploads
+const storage = multer.memoryStorage(); // Store the file in memory
+const upload = multer({ storage: storage });
+
+let data = fs.readFileSync(
+  path.join(`${__dirname}`, "/data/classification_results.csv"),
+  "utf8"
+);
+data = data.split("\n");
+let classification = {};
+for (let i = 0; i < data.length; i++) {
+  let fileName = data[i].split(",")[0];
+  let value = data[i].split(",")[1].trim();
+  classification[fileName] = value;
 }
+
 const classifer = (req, res) => {
-    const {file} = req.body 
-    res.status(400).json({
-        "status": "success",
-        "response": `${file}:${classification[file]}`
-    })
-}
+  let { originalname: file } = req.file;
+  file = file.split(".")[0];
+  res.status(200).send(`${file}:${classification[file]}`);
+};
 
-app.post('/', classifer);
+app.post("/", upload.single("inputFile"), classifer);
 
-app.listen(port, ()=>{
-    console.log(`Server started at port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Server started at port ${port}`);
+});
